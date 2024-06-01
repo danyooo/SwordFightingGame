1c66c155f0c8920f3b488d00d4718dbd9f51e8c8
danyooo <danynorby0027@gmail.com>
2024-05-31 20:55:01 -0400

    Added scaffolding to Player script ( renamed test.gd -> PlayerScript)

---
+++ PlayerScript.gd
@@ -0,0 +1,52 @@
+extends Sprite2D
+# Player variables
+# Speed
+var speed = 500
+# Rotation Speed
+var angspeed = 5
+# Direction node ( to turn the player)
+var direction = 0
+# Health varaible
+# Mass/Weight(?) For knockback when hit.
+# 'Wins' Variable
+# "UsedWords" List that stores the previously used valid word.
+# DodgeTimer variable. This will go up or down depending on the wielded swords weight/size.
+# Velocity ( to move the player)
+var velocity = Vector2.ZERO
+# Equivalent of "Draw" Function. Delta is telling it to process from the last complete frame
+func _process(delta):
+	# Rotation is the rotation speed multiplied by the chosen direction multiplied by the time between frames ( to keep a smoooth look if FPS drops)
+	rotation += angspeed * direction * delta
+	# If up is pressed..
+	if Input.is_action_pressed("Up"):
+		# Go up!
+		velocity = Vector2.UP.rotated(rotation) * speed
+# if "W" isn't actively held down don't move the character
+	else:
+		velocity= Vector2.ZERO
+	
+
+	position += velocity * delta
+# If left is pressed..
+	if Input.is_action_pressed("Left"):
+		# Go left!
+		direction = -1
+		print("moved left")
+	# if the keys aren't actively pressed down dont turn
+	else:
+		direction = 0
+		# If Right is pressed...
+	if Input.is_action_pressed("Right"):
+		# Go right!
+		direction = 1
+		print("moved right")
+#If <dodgekey> is pressed...
+
+	# Dodge! ( Snap the player forwards in the current direction they're moving in)
+	
+#If <enemysword> touches player..
+	# How much momentum was the enemysword going by?
+	# What's the enemysword's damage stat? ( based on a swords weight/size)
+	# Multiply momentum x damage and apply damage to health!
+
+
