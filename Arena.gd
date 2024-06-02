a3012c052fc20f2c18f160c42a7dc81d453dfa97
danyooo <danynorby0027@gmail.com>
2024-06-02 15:30:03 -0400

    Created arena scaffolding

---
+++ Arena.gd
@@ -0,0 +1,7 @@
+extends StaticBody2D
+# Variable "timer"
+# Function to check if anything collides with this object
+# If so, block it.
+
+#Function that, when a player is nearby for ~3 seconds, begin flashing arena red and counting down.
+# If the player remains after the timer reaches zero, damage the player over time.
