1864c2c701ec3f782f2fbb329a9060d04967f89d
danyooo <danynorby0027@gmail.com>
2024-06-01 21:01:49 -0400

    Created Camera scaffolding Renamed "sprite@D" -> "player"

---
+++ Camera2D.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+# X and Y variables ( position)
+
+# function to grab both players position and store them
+	# This could be something like 'player1X.. Player1Y..'
+# Function to calculate the the position in between the two players 
+	# The camera will take this calculation and stay there
+	# The camera should also zoom out or in depending on if the players are too far out / in to see easily
+	
