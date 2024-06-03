b98b8da4f2354fa2e8e2ba70747c928d28be0fc5
danyooo <danynorby0027@gmail.com>
2024-06-03 19:33:46 -0400

    Creates  sword scaffolding

---
+++ Sword.gd
@@ -0,0 +1,8 @@
+extends Node
+# Length and width property
+	# this will change with the word that the player has typed
+# Damage property. This should also scale with the size of the sword
+# This should move with the players mouse // left joystick
+# This should have phyiscs. A mass, and a weight. It needs to lag behind the larger the sword is to be semi realistic.
+# This should have a collision against other swords, players, and the arena.
+# This should pass along its mass/weight to the player in order to change the player's dash cooldown/distance to be more realistic.
