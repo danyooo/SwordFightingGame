7a944cee671e886870d91fe27deb1eb15e49c5cc
danyooo <danynorby0027@gmail.com>
2024-06-04 10:36:36 -0400

    Updated Player movement to be able to use diagonal movement

--- PlayerScript.gd
+++ PlayerScript.gd
@@ -2,10 +2,7 @@ extends Sprite2D
 # Player variables
 # Speed
 var speed = 500
-# Rotation Speed
-var angspeed = 5
-# Direction node ( to turn the player)
-var direction = 0
+
 # Health varaible
 # Mass/Weight(?) For knockback when hit.
 # 'Wins' Variable
@@ -15,50 +12,27 @@ var direction = 0
 var velocity = Vector2.ZERO
 # Equivalent of "Draw" Function. Delta is telling it to process from the last complete frame
 func _process(delta):
-	# Rotation is the rotation speed multiplied by the chosen direction multiplied by the time between frames ( to keep a smoooth look if FPS drops)
-	rotation += angspeed * direction * delta
-	# if any button is pressed..
-	
-		# Which button is being pressed?
+	velocity = Vector2.ZERO
 	# If up is pressed..
 	if Input.is_action_pressed("Up"):
 		# Go up!
-		velocity = Vector2.UP.rotated(rotation) * speed
-		print("moved up")
-# 
-	
-	
-
-	position += velocity * delta
+		velocity += Vector2.UP.rotated(rotation)
 # If left is pressed..
 	if Input.is_action_pressed("Left"):
 		# Go left!
-		velocity = Vector2.LEFT.rotated(rotation) * speed
-
-		print("moved left")
-	# if the keys aren't actively pressed down dont turn
-	
-		
+		velocity += Vector2.LEFT.rotated(rotation)
 		# If Right is pressed...
-	elif Input.is_action_pressed("Right"):
+	if Input.is_action_pressed("Right"):
 		# Go right!
-		velocity = Vector2.RIGHT.rotated(rotation) * speed
-		print("moved right")
-
+		velocity += Vector2.RIGHT.rotated(rotation)
 		
 		
-	elif Input.is_action_pressed("Down"):
+	if Input.is_action_pressed("Down"):
 	# Go down!
 		
-		velocity = Vector2.DOWN.rotated(rotation) * speed
-		print("moved down")
-		#var is_moving = true
-		#If a movement key isn't pressed, nothing happens
-	else:
-
-		velocity= Vector2.ZERO
-			
-			
+		velocity += Vector2.DOWN.rotated(rotation)
+		# makes diagonal movement not fast
+	position += velocity.normalized() * speed * delta 
 #If <dodgekey> is pressed...
 
 	# Dodge! ( Snap the player forwards in the current direction they're moving in)
--- player2.gd
+++ player2.gd
@@ -1,1 +1,67 @@
 extends Sprite2D
+# Player variables
+# Speed
+var speed = 500
+
+# Health varaible
+# Mass/Weight(?) For knockback when hit.
+# 'Wins' Variable
+# "UsedWords" List that stores the previously used valid word.
+# DodgeTimer variable. This will go up or down depending on the wielded swords weight/size.
+# Velocity ( to move the player)
+var velocity = Vector2.ZERO
+# Equivalent of "Draw" Function. Delta is telling it to process from the last complete frame
+func _process(delta):
+	# If up is pressed..
+	if Input.is_action_pressed("Up"):
+		# Go up!
+		velocity = Vector2.UP.rotated(rotation) * speed
+		print("moved up")
+# 
+	
+	
+
+	position += velocity * delta
+# If left is pressed..
+	if Input.is_action_pressed("Left"):
+		# Go left!
+		velocity = Vector2.LEFT.rotated(rotation) * speed
+
+		print("moved left")
+	# if the keys aren't actively pressed down dont turn
+	
+		
+		# If Right is pressed...
+	elif Input.is_action_pressed("Right"):
+		# Go right!
+		velocity = Vector2.RIGHT.rotated(rotation) * speed
+		print("moved right")
+
+		
+		
+	elif Input.is_action_pressed("Down"):
+	# Go down!
+		
+		velocity = Vector2.DOWN.rotated(rotation) * speed
+		print("moved down")
+		#var is_moving = true
+		#If a movement key isn't pressed, nothing happens
+	else:
+
+		velocity= Vector2.ZERO
+			
+			
+#If <dodgekey> is pressed...
+
+	# Dodge! ( Snap the player forwards in the current direction they're moving in)
+	
+#If <enemysword> touches player..
+	# How much momentum was the enemysword going by?
+	# What's the enemysword's damage stat? ( based on a swords weight/size)
+	# Multiply momentum x damage and apply damage to health!
+	
+# function that, while on the choosing worde scene, takes the players inputs into a list
+# After taking them into a list, merge these into a single string to form a word.
+# Pass this onto the word node.
+
+
