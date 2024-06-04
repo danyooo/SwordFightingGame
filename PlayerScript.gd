01a75b0c83c46992216dbdee06e47a2805b25bff
SpaghettiMonster <norbybutora123@gmail.com>
2024-06-04 11:40:46 -0400

    Player movement wip  inertia and acceleration

--- PlayerScript.gd
+++ PlayerScript.gd
@@ -1,22 +1,25 @@
-extends Sprite2D
+extends CharacterBody2D
 # Player variables
 # Speed
-var speed = 500
-
+const Maxspeed = 500
+#acceleration
+const accel = 1500
+const friction = 600
 # Health varaible
 # Mass/Weight(?) For knockback when hit.
 # 'Wins' Variable
 # "UsedWords" List that stores the previously used valid word.
 # DodgeTimer variable. This will go up or down depending on the wielded swords weight/size.
 # Velocity ( to move the player)
-var velocity = Vector2.ZERO
+
 # Equivalent of "Draw" Function. Delta is telling it to process from the last complete frame
 func _process(delta):
-	velocity = Vector2.ZERO
+	
 	# If up is pressed..
 	if Input.is_action_pressed("Up"):
 		# Go up!
 		velocity += Vector2.UP.rotated(rotation)
+		
 # If left is pressed..
 	if Input.is_action_pressed("Left"):
 		# Go left!
@@ -32,7 +35,19 @@ func _process(delta):
 		
 		velocity += Vector2.DOWN.rotated(rotation)
 		# makes diagonal movement not fast
-	position += velocity.normalized() * speed * delta 
+	position += velocity.normalized() * Maxspeed * delta 
+	if velocity == Vector2.ZERO:
+	
+		if velocity.length() > (friction * delta):
+			print("h")
+			#normalized means it gets rid of speed, and only uses direction
+			velocity -= velocity.normalized() * (friction * delta)
+		#else:
+			#velocity = Vector2.ZERO
+		else:
+			velocity += (Vector2.ZERO * accel * delta)
+			velocity = velocity.limit_length(Maxspeed)
+	move_and_slide()
 #If <dodgekey> is pressed...
 
 	# Dodge! ( Snap the player forwards in the current direction they're moving in)
