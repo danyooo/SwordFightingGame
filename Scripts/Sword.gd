921120a5aa555ee2258103fd22257e24103c1faf
danyooo <danynorby0027@gmail.com>
2024-06-04 11:02:11 -0400

    Reorgranized sword script into scripts folder

--- Scripts/Sword.gd
+++ Scripts/Sword.gd
@@ -1,1 +1,31 @@
 extends Sprite2D
+
+
+
+#Sword Stat class here
+#Sword's Length
+#Sword's Width
+#Sword's Weight Stat
+#Sword's Durability Stat
+#Sword momentum stat(0 default)
+#Sword damage stat(0 default)
+
+#Sword's math for physics/momentum(if u can find any)
+
+#When letter typed, Sword Length/width++
+
+#otherwise length --, width --
+
+#Detection for setting Sword's durability lower when hit
+
+# Detection for amount of momentum behind enemy sword when hitting player's sword, deals certain amount of damage
+
+#Collision for hitting wall(?)
+
+#Detection for mouse movement/joystick movement, sword moves with where it is aimed
+
+#Weight stat slows down momentum
+
+#Momentum increase as sword is swinging
+
+#Sword's weight increases damaage
