b3f84c191c2cca97e920d64eae31948eb17b6628
Spaghettimonstar <norbybutora123@gmail.com>
2024-06-09 20:21:17 -0400

    Attempted to figure out how to make "Tester" disappear once health reaches zero, cant figure it out.

--- scenes/Area2D.gd
+++ scenes/Area2D.gd
@@ -1,10 +1,15 @@
 extends Area2D
 var health := 100;
 
-
+#Detects if something collided with the tester's body
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	#If collided;
 	if body_entered:
+		#Subtract health
 		health = health - 99
 		print(health)
+		#If health is less then 1, print game over
 		if health < 1:
 				print("game over")
+				hide()
+				queue_free()
