d99d8a12afd23076454aef54bf0b7994a8305150
SpaghettiMonster <norbybutora123@gmail.com>
2024-06-13 10:45:02 -0400

    Uploading so Dan & me can try to sort out tihs problem

--- scenes/Tester.gd
+++ scenes/Tester.gd
@@ -1,10 +1,15 @@
 class_name TesterHurtbox
 extends RigidBody2D
 
-
-var EnemyHurtboxy = EnemyHurtbox.new()
-
-func clear():
-	if EnemyHurtboxy.clearcheck == true:
-		print("a")
-		queue_free()
+var test = 100
+#var EnemyHurtboxy = EnemyHurtbox.new()
+#
+#func clear():
+	#if EnemyHurtboxy.clearcheck == true:
+		#print("a")
+		#queue_free()
+func funytest():
+	test - 1
+	if test < 100:
+		hide()
+	
