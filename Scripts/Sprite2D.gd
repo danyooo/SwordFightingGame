255cc9ba65e83536056c4fad7e158e3e6b189b3a
danyooo <danynorby0027@gmail.com>
2024-06-15 16:31:21 -0400

    Revert "Merge branch 'Add-sword-hit/hurtbox' into Player"
    This reverts commit 3d480186422e1e32817e3697c7220e3633e3e664, reversing
    changes made to 21ab93519a0e0688983a19e46fbf165170b0aef9.

--- Scripts/Sprite2D.gd
+++
@@ -1,9 +0,0 @@
-class_name Hurtbox
-extends Area2D
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- Sword.gd
+++ Sword.gd
@@ -1,29 +1,8 @@
 extends Node
+# Length and width property
+	# this will change with the word that the player has typed
+# Damage property. This should also scale with the size of the sword
+# This should move with the players mouse // left joystick
+# This should have phyiscs. A mass, and a weight. It needs to lag behind the larger the sword is to be semi realistic.
 # This should have a collision against other swords, players, and the arena.
 # This should pass along its mass/weight to the player in order to change the player's dash cooldown/distance to be more realistic.
-
-#Sword Stat class here
-#Sword's Length (Changes with WordTyped by player)
-#Sword's Width(Changes with WordTyped by player)
-#Sword's Weight Stat
-#Sword's Durability Stat
-# This should have phyiscs. A mass, and a weight. It needs to lag behind the larger the sword is to be semi realistic.
-#Sword momentum stat(0 default) increasees as the sword is swung (But negatively multiplied by the swords size?
-#Sword damage stat(0 default) Scales with size of sword
-
-#Sword's math for physics/momentum(if u can find any)
-
-#When letter typed, Sword Length/width++
-
-#otherwise length --, width --
-
-#Weight stat slows down momentum
-
-#Momentum increase as sword is swinging
-
-#Sword's weight increases damaage
-# Draw function party
-# This should move with the players mouse // left joystick. Sword is weighed down by its size.
-#Collision check  on other swords, players, arena.
-# Detection for amount of momentum behind enemy sword when hitting player's sword, deals certain amount of damage	
-	#Detection for setting Sword's durability lower when hit
