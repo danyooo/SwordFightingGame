0acd0845eab9aec991e243f37f9507610988f45c
danyooo <42193011+danyooo@users.noreply.github.com>
2024-06-15 15:54:17 -0400

    Revert "Add player updates to sword"

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -8,52 +8,23 @@ const accel = 1800
 # friction; stopping power
 const friction = 400
 # Health variable
-var health = 100
 # Mass/Weight(?) For knockback when hit.
-var mass = 20
-var weight = 20
 # 'Wins' Variable
-var wins = 0
 # "UsedWords" List that stores the previously used valid word.
-var UsedWords = []
 # DodgeTimer variable. This will go up or down depending on the wielded swords weight/size.
-var dodgecooldown = 3 #  10 seconds by default. * SwordWeight?
-# Dodge is available by default
-var dodgeready = true
-var dodgeDist = 20 # 20 default affected by swordWeight.
-# Add a new velocity just for dodging
-var dodgeVelocity: Vector2
-# Collision variable
-var being_collided:bool
-# Knockback variable. added this so that the players dont both knock each other back at the same time
-var knockback = Vector2.ZERO
+# Velocity ( to move the player)
 # Equivalent of "Draw" Function. Delta is telling it to process from the last complete frame
 func _physics_process(delta):
-	var knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	move(delta)
-	#if a collision occurs
-	if being_collided == true:
-		# What did you collide with?
-		for i in get_slide_collision_count():
-			var collision = get_slide_collision(i)
-			# How fast was the collider moving?
-			collision.get_collider_velocity() # Get the colliders velocity
-			# Get pushed back 
-			knockback = collision.get_collider_velocity()
-			velocity = knockback * input
-
-
-	#If the dodge button is pressed AND the cooldown has expired..
 	
-	if Input.is_action_pressed("Dodge") && dodgeready == true:
-		# This will depend on the sword the player is wielding. The larger and heavier it is, the less distance and higher cooldown.
-		# Dodge!
-		Dodge(input,dodgeDist, delta)
-		# Dodge goes on cooldown
-		$dodgecooldowntimer.set_wait_time(dodgecooldown)
-		$dodgecooldowntimer.start()
-		dodgeready = false
+#If <dodgekey> is pressed...
 
+	# Dodge! ( Snap the player forwards in the current direction they're moving in)
+	# This will depend on the sword the player is wielding. The larger and heavier it is, the less distance and higher cooldown.
+#If <enemysword> touches player..
+	# How much momentum was the enemysword going by?
+	# What's the enemysword's damage stat? ( based on a swords weight/size)
+	# Multiply momentum x damage and apply damage to health!
 
 var input = Vector2.ZERO
 # Detect which way the player is moving (in number format)
@@ -82,41 +53,10 @@ func move(delta):
 	else:
 		# Move in the direction the player wants to move
 		velocity += (input * accel * delta)
-		# To dodge past max speed, add dodgevelocity ( so dodging isn't capped)
-		velocity = velocity.limit_length(Maxspeed) + dodgeVelocity
-		dodgeVelocity = Vector2.ZERO
+		velocity = velocity.limit_length(Maxspeed)
 	move_and_slide()
-func wordChoose():
-	pass
-	# function that, while on the choosing worde scene, takes the players inputs into a list
-	# After taking them into a list, merge these into a single string to form a word.
-	# Pass this onto the word node.
-func Dodge(Pinput,distance, delta):
-	# jump forwards
-	print("Dodged!")
-	dodgeVelocity = ( Pinput * accel *  distance *  delta)
-#If <enemysword> touches player..
-	# How much momentum was the enemysword going by?
-	# What's the enemysword's damage stat? ( based on a swords weight/size)
-	# Multiply momentum x damage and apply damage to health!
+# function that, while on the choosing worde scene, takes the players inputs into a list
+# After taking them into a list, merge these into a single string to form a word.
+# Pass this onto the word node.
 
 
-
-
-
-	#When the tiemr is done
-func _on_dodgecooldowntimer_timeout():
-	# Set dodge ready to true
-	print("Dodge Ready! for keyboard!")
-	dodgeready = true
-	# stop the timer
-	$dodgecooldowntimer.stop()
-
-# on collisison
-func _on_area_2d_area_entered(area):
-	print("player 1 collission!")
-	being_collided = true
-	
-func _on_collision_detector_area_exited(area):
-	print("no collissions for P1")
-	var being_collided = false
